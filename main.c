b873deba1a2691f50179db49f2aa6cbbfb3efdb4
arseny.saw4enko@gmail.com <arseny.saw4enko@gmail.com>
2023-08-16 15:34:09 +0300

    fixed crash when no domain found + domain alliases

--- main.c
+++ main.c
@@ -2,60 +2,106 @@
 
 #include <conio.h>
 #include <WinSock2.h>
+#include <WS2tcpip.h>
+
+#define MAX(a, b) ((a > b) ? a : b)
+
+void
+init_ws()
+{
+  static WSADATA wsaData;
+  register int code;
+
+  code = WSAStartup(MAKEWORD(1, 1), &wsaData);
+
+  if (code != 0) {
+    fputs("Failed to initialize WSA\n", stderr);
+    exit(1);
+  }
+}
 
 void
 halt()
 {
-  (void)fputs("Press any key to continue...", stdout);
-  (void)_getch();
-  (void)fputs("\n", stdout);
+  fputs("Press any key to continue...", stdout);
+  _getch();
+  fputs("\n", stdout);
+}
+
+void
+dnslookup(const char* domain)
+{
+  struct hostent* hostent;
+  char* alias;
+  char addr[MAX(INET6_ADDRSTRLEN, INET_ADDRSTRLEN)];
+
+  hostent = gethostbyname(domain);
+
+  fputs(domain, stdout);
+  fputs("\n", stdout);
+
+  if (hostent == NULL) {
+    fputs("  [not found]\n", stdout);
+    return;
+  }
+
+  fputs("  aliasses:\n", stdout);
+  if (hostent->h_aliases == NULL || hostent->h_aliases[0] == NULL) {
+    fputs("    [none]\n", stdout);
+    goto skip_aliases;
+  }
+
+  for (char** p = hostent->h_aliases; *p; ++p) {
+    alias = *p;
+
+    fputs("    ", stdout);
+    fputs(alias, stdout);
+    fputs("\n", stdout);
+  }
+
+  skip_aliases:
+
+  fputs("  addresses:\n", stdout);
+  if (hostent->h_addr_list == NULL || hostent->h_addr_list[0] == NULL) {
+    fputs("    [none]\n", stdout);
+    return;
+  }
+
+  for (char** p = hostent->h_addr_list; *p; ++p) {
+    addr[0] = 0;
+    (void)inet_ntop(hostent->h_addrtype, *p, addr, sizeof(addr));
+
+    if (addr[0] == 0) {
+      fputs("    [failed]\n", stdout);
+      continue;
+    }
+
+    fputs("    ", stdout);
+    fputs(addr, stdout);
+    fputs("\n", stdout);
+  }
 }
 
 int
 main(int argc, char** argv)
 {
-  WSADATA wsaData;
-  int code;
-  char* domain;
-  struct hostent* h;
-  char* addr;
-  char addr_str[3 + 1 + 3 + 1 + 3 + 1 + 3 + 1];
-
   if (argc < 2) {
-    (void)fputs("No domain name\n", stderr);
+    fputs("No domain name\n", stderr);
+
     halt();
+
     return 1;
   }
   
-  code = WSAStartup(MAKEWORD(1, 1), &wsaData);
+  init_ws();
 
-  if (code != 0) {
-    (void)fputs("Failed to startup WSA\n", stderr);
-    halt();
-    return 1;
-  }
-
-  for (char** arg_ptr = &argv[1]; *arg_ptr; ++arg_ptr) {
-    domain = *arg_ptr;
-
-    (void)fputs(domain, stdout);
-    (void)fputs("\n", stdout);
-
-    h = gethostbyname(domain);
-
-    for (char** addr_ptr = &h->h_addr_list[0]; *addr_ptr; ++addr_ptr) {
-      addr = *addr_ptr;
-
-      sprintf(addr_str, "%hhu.%hhu.%hhu.%hhu", addr[0], addr[1], addr[2], addr[3]);
-
-      (void)fputs("\t", stdout);
-      (void)fputs(addr_str, stdout);
-      (void)fputs("\n", stdout);
-    }
+  for (char** arg = &argv[1]; *arg; ++arg) {
+    dnslookup(*arg);
   }
 
   halt();
 
-  (void)WSACleanup();
+  WSACleanup();
+
   return 0;
 }
